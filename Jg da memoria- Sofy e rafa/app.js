be005ba05f84824be9a43c79451973c1fdc7c4aa
rafaelly-2a <160231003+rafaelly-2a@users.noreply.github.com>
2024-11-06 08:43:51 -0300

    Add files via upload

---
+++ Jg da memoria- Sofy e rafa/app.js
@@ -0,0 +1,26 @@
+function criaCartao(pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+    <div class="cartao__frente>
+    </div>
+    <div class="cartao__verso">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
+
+}
---
+++ Jg da memoria- Sofy e rafa/imagens.js
@@ -0,0 +1,19 @@
+criaCartao(
+    '<img src="img/1.jpg">'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Como se diz oi em Inglês?',
+
+)
